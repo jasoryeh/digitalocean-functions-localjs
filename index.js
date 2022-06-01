2800ec4ac98bde5a8399f8e99c5e318cd85fc75f
Jason Ho <git@hogt.me>
2022-06-01 08:50:44 -0700

    timing? and cleanup

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function inVM(script, args, environment) {
     vm.createContext(context);
     
     let scr = `let entry = require('${script}'); backContext.returned = entry.main(backContext.args);`;
+    var start = process.hrtime();
     vm.runInContext(scr, context, {
         lineOffset: 0,
         columnOffset: 0,
@@ -26,6 +27,8 @@ async function inVM(script, args, environment) {
         timeout: 30000
     })
     context.backContext.returned = await context.backContext.returned;
+    var end = process.hrtime(start);
+    console.log(`Exec: ${end[0]}s ${end[1]/1000000}ms`)
     process.eenv = envCopy;
     return context.backContext;
 }
@@ -42,6 +45,10 @@ module.exports.run = function (port = 80, projectYMLFile = './project.yml', pack
 
     for (let package of config.packages) {
         for (let action of package.actions) {
+            if (!action.runtime.includes("nodejs")) {
+                console.log("Skipping action " + action.name + " since it is a nodejs runtime.");
+                continue;
+            }
             let subdirectory = package.name + "/" + action.name;
             let actionLocation = packagesDirectory + "/" + subdirectory;
             let fsPackage = fs.readFileSync(actionLocation + "/package.json", "utf-8");
@@ -63,12 +70,15 @@ module.exports.run = function (port = 80, projectYMLFile = './project.yml', pack
                 });
                 Object.assign(args, req.query);
                 Object.assign(args, req.body);
-
+                
                 let specificEnvironment = {};
                 Object.assign(specificEnvironment, environment);
                 Object.assign(specificEnvironment, action.environment);
-    
+                
+                var start = process.hrtime();
                 let runInVM = await inVM(entrypointScript, args, specificEnvironment);
+                var end = process.hrtime(start);
+                console.log(`Processed ${req.url} -> ${actionLocation} -> ${path} | ${end[0]}s ${end[1]/1000000}ms`);
                 let exec = runInVM.returned;
                 res.header("Content-Type",'application/json');
                 res.status(exec.body ? (exec.status ?? 500) : 204).send(JSON.stringify(exec.body, null, 4));
